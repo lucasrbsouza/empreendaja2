8ae3d2127e46ea6053ca37af17057818c96df75a
lucasrbsouza <joselucastrbssilvasouza@gmail.com>
2025-05-26 22:02:31 -0300

    fix problems

--- lib/views/profile_screen/edit_profile_screen.dart
+++ lib/views/profile_screen/edit_profile_screen.dart
@@ -13,27 +13,30 @@ class EditProfileScreen extends StatelessWidget {
         title: boldText(text: editProfile, size: 16.0),
         actions: [TextButton(onPressed: () {}, child: normalText(text: save))],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Image.network(
-              "https://imgs.search.brave.com/x55EukpOKvsr84rxXNZRs0JTUu070OEVsbR1s1sj4o0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vd0dnZmRx/bEZxYU1lbloyNk1G/MFdvZ2JHdC1kanBU/T0tfUHp3TGFOMWxQ/cy9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkw/TkM1bS9kR05rYmk1/dVpYUXZhbkJuL0x6/QXdMelkwTHpZM0x6/STMvTHpNMk1GOUdY/elkwTmpjeS9Oek0y/WDFVMWEzQmtSM001/L2EyVlZiR3c0UTFK/Uk0zQXovV1dGRmRq/Sk5ObkZyVmxrMS9M/bXB3Wnc",
-              width: 150,
-            ).box.roundedFull.clip(Clip.antiAlias).make(),
-            10.heightBox,
-            ElevatedButton(
-              onPressed: () {},
-              child: normalText(text: changeImage, color: fontGrey),
-            ),
-            10.heightBox,
-            const Divider(color: white),
-            customTextField(label: name, hint: "eg. Lucass Dev"),
-            10.heightBox,
-            customTextField(label: password, hint: passowordHint),
-            10.heightBox,
-            customTextField(label: confirmPassword, hint: passowordHint),
-          ],
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Image.network(
+                "https://imgs.search.brave.com/x55EukpOKvsr84rxXNZRs0JTUu070OEVsbR1s1sj4o0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vd0dnZmRx/bEZxYU1lbloyNk1G/MFdvZ2JHdC1kanBU/T0tfUHp3TGFOMWxQ/cy9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkw/TkM1bS9kR05rYmk1/dVpYUXZhbkJuL0x6/QXdMelkwTHpZM0x6/STMvTHpNMk1GOUdY/elkwTmpjeS9Oek0y/WDFVMWEzQmtSM001/L2EyVlZiR3c0UTFK/Uk0zQXovV1dGRmRq/Sk5ObkZyVmxrMS9M/bXB3Wnc",
+                width: 150,
+              ).box.roundedFull.clip(Clip.antiAlias).make(),
+              10.heightBox,
+              ElevatedButton(
+                onPressed: () {},
+                child: normalText(text: changeImage, color: fontGrey),
+              ),
+              10.heightBox,
+              const Divider(color: white),
+              customTextField(label: name, hint: "eg. Lucass Dev"),
+              10.heightBox,
+              customTextField(label: password, hint: passowordHint),
+              10.heightBox,
+              customTextField(label: confirmPassword, hint: passowordHint),
+            ],
+          ),
         ),
       ),
     );
--- lib/views/profile_screen/profile_screen.dart
+++ lib/views/profile_screen/profile_screen.dart
@@ -30,42 +30,45 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          ListTile(
-            leading:
-                Image.network(
-                  "https://imgs.search.brave.com/x55EukpOKvsr84rxXNZRs0JTUu070OEVsbR1s1sj4o0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vd0dnZmRx/bEZxYU1lbloyNk1G/MFdvZ2JHdC1kanBU/T0tfUHp3TGFOMWxQ/cy9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkw/TkM1bS9kR05rYmk1/dVpYUXZhbkJuL0x6/QXdMelkwTHpZM0x6/STMvTHpNMk1GOUdY/elkwTmpjeS9Oek0y/WDFVMWEzQmtSM001/L2EyVlZiR3c0UTFK/Uk0zQXovV1dGRmRq/Sk5ObkZyVmxrMS9M/bXB3Wnc",
-                ).box.roundedFull.clip(Clip.antiAlias).make(),
-            title: boldText(text: "vendor name"),
-            subtitle: normalText(text: "vendor@gamil.com"),
-          ),
-          const Divider(),
-          10.heightBox,
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: List.generate(
-                profileButtonsIcons.length,
-                (index) => ListTile(
-                  onTap: () {
-                    switch (index) {
-                      case 0:
-                        Get.to(() => const ShopSetting());
-                        break;
-                      case 1:
-                        Get.to(() => const MessagesScreen());
-                        break;
-                      default:
-                    }
-                  },
-                  leading: Icon(profileButtonsIcons[index], color: white),
-                  title: normalText(text: profileButtonsTitles[index]),
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          children: [
+            ListTile(
+              leading:
+                  Image.network(
+                    "https://imgs.search.brave.com/x55EukpOKvsr84rxXNZRs0JTUu070OEVsbR1s1sj4o0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vd0dnZmRx/bEZxYU1lbloyNk1G/MFdvZ2JHdC1kanBU/T0tfUHp3TGFOMWxQ/cy9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTkw/TkM1bS9kR05rYmk1/dVpYUXZhbkJuL0x6/QXdMelkwTHpZM0x6/STMvTHpNMk1GOUdY/elkwTmpjeS9Oek0y/WDFVMWEzQmtSM001/L2EyVlZiR3c0UTFK/Uk0zQXovV1dGRmRq/Sk5ObkZyVmxrMS9M/bXB3Wnc",
+                  ).box.roundedFull.clip(Clip.antiAlias).make(),
+              title: boldText(text: "vendor name"),
+              subtitle: normalText(text: "vendor@gamil.com"),
+            ),
+            const Divider(),
+            10.heightBox,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: List.generate(
+                  profileButtonsIcons.length,
+                  (index) => ListTile(
+                    onTap: () {
+                      switch (index) {
+                        case 0:
+                          Get.to(() => const ShopSetting());
+                          break;
+                        case 1:
+                          Get.to(() => const MessagesScreen());
+                          break;
+                        default:
+                      }
+                    },
+                    leading: Icon(profileButtonsIcons[index], color: white),
+                    title: normalText(text: profileButtonsTitles[index]),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
